b3e7d26a271fb11be857439ba21064b217371497
Raul Jimenez <rauljmz@gmail.com>
2019-08-01 15:28:01 +0100

    Update from base (#1)
    * Updated driver and added support for Chrome Options
    
    * Updated README
    
    * Fix to Invoke-SeScreenshot to return Base64EncodedString
    
    * Add a new cmdlet 'Get-SeKeys' to list supported keys, update 'Send-SeKeys' cmdlet to support '{{KeyName}}' format, e.g. {{Enter}}
    
    * update 'Get-SeKeys' to output the object string
    
    * new cmdlet 'Wait-SeElementExists' to wait for a element until it presents
    
    * update readme

--- Selenium.psm1
+++ Selenium.psm1
@@ -1,7 +1,20 @@
 [System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\assemblies\WebDriver.dll")
 [System.Reflection.Assembly]::LoadFrom("$PSScriptRoot\assemblies\WebDriver.Support.dll")
 function Start-SeChrome {
-    New-Object -TypeName "OpenQA.Selenium.Chrome.ChromeDriver"
+    Param(
+        [Parameter(Mandatory = $false)]
+        [array]$Arguments,
+        [switch]$HideVersionHint
+    )
+    if($Arguments) {
+        $Chrome_Options = New-Object -TypeName "OpenQA.Selenium.Chrome.ChromeOptions"
+        $Chrome_Options.AddArguments($Arguments)
+    }
+    if(!$HideVersionHint)
+    {
+        Write-Host "Download the right chromedriver from 'http://chromedriver.chromium.org/downloads'" -ForegroundColor Yellow
+    }
+    New-Object -TypeName "OpenQA.Selenium.Chrome.ChromeDriver" -ArgumentList $Chrome_Options
 }
 
 function Start-SeFirefox {
@@ -113,9 +126,18 @@ function Invoke-SeClick {
     
 }
 
+function Get-SeKeys {
+    
+    [OpenQA.Selenium.Keys] | Get-Member -MemberType Property -Static | Select-Object -Property Name,@{N = "ObjectString"; E = {"[OpenQA.Selenium.Keys]::$($_.Name)"}}
+}
+
 function Send-SeKeys {
     param([OpenQA.Selenium.IWebElement]$Element, [string]$Keys)
-
+    
+    foreach($Key in @(Get-SeKeys).Name)
+    {
+        $Keys = $Keys -replace "{{$Key}}", [OpenQA.Selenium.Keys]::$Key
+    }
     $Element.SendKeys($Keys)
 }
 
@@ -156,8 +178,10 @@ function Invoke-SeScreenshot {
     param($Driver, [Switch]$AsBase64EncodedString)
 
     $Screenshot = [OpenQA.Selenium.Support.Extensions.WebDriverExtensions]::TakeScreenshot($Driver)
-    if ($AsBase64String) {
+    if ($AsBase64EncodedString) {
         $Screenshot.AsBase64EncodedString
+    } else {
+        $Screenshot
     }
 }
 
@@ -174,3 +198,29 @@ function Save-SeScreenshot {
             $Screenshot.SaveAsFile($Path, $ImageFormat)
         }
 }
+
+function Wait-SeElementExists {
+    param(
+        $Driver,
+        $Timeout = 30,
+        $Id,
+        $Name
+    )
+    if($Id)
+    {
+        $TargetElement = [OpenQA.Selenium.By]::Id($Id)
+    }
+    elseif($Name)
+    {
+        $TargetElement = [OpenQA.Selenium.By]::Name($Name)
+    }
+    else
+    {
+        throw "Please specify -Id or -Name"
+    }
+    $WebDriverWait = New-Object OpenQA.Selenium.Support.UI.WebDriverWait $Driver, ($Timeout * 10000000) # ticks
+    $Condition = [OpenQA.Selenium.Support.UI.ExpectedConditions]::ElementExists($TargetElement)
+    $WebDriverWait.Until($Condition)
+}
+
+
